137b80e68a61174925f6afa4e3c64c7e97cb21a4
Emmanuel Vernet <vernet.emmanuel@gmail.com>
2024-11-12 16:29:18 +0100

    added frontend

---
+++ app/helpers/articles_contoller_helper.rb
@@ -0,0 +1,2 @@
+module ArticlesContollerHelper
+end
---
+++ test/controllers/articles_contoller_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class ArticlesContollerControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
